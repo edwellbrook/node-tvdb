f3f9eb6360e8a7a8676511d64339f0bc07acc262
Ed Wellbrook <edwellbrook@gmail.com>
2015-05-05 21:37:43 +0100

    tidied test runner

--- test/runner.js
+++ test/runner.js
@@ -1,36 +1,35 @@
 var NativeClient = require("..");
 var CompatClient = require("../compat");
 
-describe("Native client", function() {
-    require("./tests/module")(NativeClient);
-    require("./tests/language")(NativeClient);
-    require("./tests/time")(NativeClient);
-    require("./tests/update")(NativeClient);
-    require("./tests/utils")(NativeClient);
-    require("./tests/episode")(NativeClient);
+function test(client) {
+    [
+        "./tests/module",
+        "./tests/language",
+        "./tests/time",
+        "./tests/update",
+        "./tests/utils",
+        "./tests/episode"
+    ].forEach(function(i) {
+        require(i)(client);
+    });
 
     describe("Series endpoints", function() {
-        require("./tests/find-actors")(NativeClient);
-        require("./tests/find-banners")(NativeClient);
-        require("./tests/search-by-id")(NativeClient);
-        require("./tests/search-by-name")(NativeClient);
-        require("./tests/search-by-remote")(NativeClient);
+        [
+            "./tests/find-actors",
+            "./tests/find-banners",
+            "./tests/search-by-id",
+            "./tests/search-by-name",
+            "./tests/search-by-remote"
+        ].forEach(function(i) {
+            require(i)(client);
+        });
     });
+}
+
+describe("Native client", function() {
+    test(NativeClient);
 });
 
 describe("Compatibility client", function() {
-    require("./tests/module")(CompatClient);
-    require("./tests/language")(CompatClient);
-    require("./tests/time")(CompatClient);
-    require("./tests/update")(CompatClient);
-    require("./tests/utils")(CompatClient);
-    require("./tests/episode")(CompatClient);
-
-    describe("Series endpoints", function() {
-        require("./tests/find-actors")(CompatClient);
-        require("./tests/find-banners")(CompatClient);
-        require("./tests/search-by-id")(CompatClient);
-        require("./tests/search-by-name")(CompatClient);
-        require("./tests/search-by-remote")(CompatClient);
-    });
+    test(CompatClient);
 });
