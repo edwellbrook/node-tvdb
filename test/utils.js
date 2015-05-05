1ff3d51d31257742e373453390dfe2823c70b60e
Ed Wellbrook <edwellbrook@gmail.com>
2015-05-05 20:01:36 +0100

    added test for parsePipeList util

---
+++ test/utils.js
@@ -0,0 +1,13 @@
+var assert = require("assert");
+var API_KEY = process.env.TVDB_KEY;
+var TVDBClient = require("..");
+
+describe("Utility Functions", function() {
+
+    it("should parse a pipe list to a javascript array", function() {
+        var input = "abc|def|ghi|jkl|";
+        var output = TVDBClient.utils.parsePipeList(input);
+
+        assert.deepEqual(["abc", "def", "ghi", "jkl"], output);
+    });
+});
