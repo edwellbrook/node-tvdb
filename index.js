24df1803a273ff730255e00c9f8c9ceccbed28d8
Yusi <ronnie@62-210-204-234.online.net>
2015-10-13 19:07:31 +0200

    Added getEpisodesById to get only episodes from all-api

--- index.js
+++ index.js
@@ -111,6 +111,24 @@ class Client {
     }
 
     /**
+     * Get all episodes from series by id
+     *
+     * @param {Number|String} id
+     * @param {Function} [callback]
+     * @return {Promise} promise
+     * @api public
+     */
+
+    getEpisodesById(id, callback) {
+        const url = `${this.baseURL}/api/${this.token}/series/${id}/all/${this.language}.xml`;
+
+        return sendRequest({url: url, lang: this.language}, RESPONSE_TYPE.XML, function(response, done) {
+            response = (response && response.Data) ? response.Data.Episode : null;
+            done(!response || Array.isArray(response) ? response : [response]);
+        }, callback);
+    }
+
+    /**
      * Get basic series information by id
      *
      * http://www.thetvdb.com/wiki/index.php?title=API:Base_Series_Record
