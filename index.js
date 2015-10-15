b8ba47cc546dd9967c4d625a5841db1d7792f27f
Ed Wellbrook <edwellbrook@gmail.com>
2015-10-15 22:20:01 +0100

    tidy `getEpisodesById`

--- index.js
+++ index.js
@@ -111,28 +111,6 @@ class Client {
     }
 
     /**
-     * Get all episodes from series by id
-     *
-     * http://www.thetvdb.com/wiki/index.php?title=API:Full_Series_Record
-     *
-     * Same as getSeriesById, but we only return all of the Episode elements.
-     *
-     * @param {Number|String} id
-     * @param {Function} [callback]
-     * @return {Promise} promise
-     * @api public
-     */
-
-    getEpisodesById(id, callback) {
-        const url = `${this.baseURL}/api/${this.token}/series/${id}/all/${this.language}.xml`;
-
-        return sendRequest({url: url, lang: this.language}, RESPONSE_TYPE.XML, function(response, done) {
-            response = (response && response.Data) ? response.Data.Episode : null;
-            done(!response || Array.isArray(response) ? response : [response]);
-        }, callback);
-    }
-
-    /**
      * Get basic series information by id
      *
      * http://www.thetvdb.com/wiki/index.php?title=API:Base_Series_Record
@@ -203,6 +181,26 @@ class Client {
             done(response ? response.Data.Series : null);
         }, callback);
     }
+
+   /**
+    * Get all episodes by series id
+    *
+    * http://www.thetvdb.com/wiki/index.php?title=API:Full_Series_Record
+    *
+    * @param {Number|String} id
+    * @param {Function} [callback]
+    * @return {Promise} promise
+    * @api public
+    */
+
+   getEpisodesById(id, callback) {
+       const url = `${this.baseURL}/api/${this.token}/series/${id}/all/${this.language}.xml`;
+
+       return sendRequest({url: url, lang: this.language}, RESPONSE_TYPE.XML, function(response, done) {
+           response = (response && response.Data) ? response.Data.Episode : null;
+           done(!response || Array.isArray(response) ? response : [response]);
+       }, callback);
+   }
 
     /**
      * Get series actors by series id
